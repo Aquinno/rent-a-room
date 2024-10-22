ad5753501a8985769e3aa828de2aa2a2c87b6222
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-10-22 00:48:10 -0300

    Add files via upload

---
+++ ReservaDeQuartos/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ReservaDeQuartos {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("How many rooms will be rented? (1 ~ 10): ");
+            int num = int.Parse(Console.ReadLine());
+            Rooms[] vect = new Rooms[11]; //Creating the array of rooms
+            // I used 11 because the rooms are from 1 to 10, it's easier to manage the array
+            // If someone use the room 0, congratulations, you found a Easter Egg
+            for (int i = 0; i < num; i++) {
+                Console.WriteLine($"Rent #{i+1}: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                byte room = byte.Parse(Console.ReadLine());
+                if (vect[room] != null) { //Checking if the room is avaiable
+                    Console.WriteLine("Room already rented.");
+                    Console.WriteLine(" ");
+                    i--; // Decrement i to repeat the loop
+                    continue;
+                }
+                else {
+                    vect[room] = new Rooms { Name = name, Email = email, Room = room };
+                    Console.WriteLine(" ");
+                }
+            }
+            Console.WriteLine("Busy Rooms:"); //Showing the rented rooms
+            for (int i = 0; i < vect.Length; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine($"{vect[i].Room}: {vect[i].Name}, {vect[i].Email}");
+                }
+            }
+        }
+    }
+}
---
+++ ReservaDeQuartos/Rooms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaDeQuartos {
+    internal class Rooms {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public byte Room { get; set; }
+    }
+}
